1af182b877ee78ff843e7d68512041076e04415a
Alberto Domínguez <84203914+stoker2010@users.noreply.github.com>
2025-11-19 11:44:58 +0100

    Add constants for Tarifas 2.0TD integration
    Define constants for Tarifas 2.0TD integration.

---
+++ custom_components/tarifas_20td/const.py
@@ -0,0 +1,17 @@
+"""Constantes para la integración Tarifas 2.0TD."""
+
+DOMAIN = "tarifas_20td"
+
+# Configuración
+CONF_GRID_SENSOR = "grid_sensor"
+CONF_SOLAR_SENSOR = "solar_sensor"
+CONF_POWER_VALLE = "power_valle"
+CONF_POWER_PUNTA = "power_punta"
+CONF_WORKDAY = "workday_entity"
+
+DEFAULT_WORKDAY = "binary_sensor.workday_sensor"
+
+# Tramos
+TRAMO_VALLE = "valle"
+TRAMO_LLANA = "llana"
+TRAMO_PUNTA = "punta"
