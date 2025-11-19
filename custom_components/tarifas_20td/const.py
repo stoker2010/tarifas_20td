d75b627aa5047a4bcb65db19edc2426130172d50
Alberto Domínguez <84203914+stoker2010@users.noreply.github.com>
2025-11-19 19:04:23 +0100

    Update const.py

--- custom_components/tarifas_20td/const.py
+++ custom_components/tarifas_20td/const.py
@@ -2,12 +2,18 @@
 
 DOMAIN = "tarifas_20td"
 
-# Configuración
+# Configuración Hogar
 CONF_GRID_SENSOR = "grid_sensor"
 CONF_SOLAR_SENSOR = "solar_sensor"
 CONF_POWER_VALLE = "power_valle"
 CONF_POWER_PUNTA = "power_punta"
 CONF_WORKDAY = "workday_entity"
+
+# Configuración Termo
+CONF_TERMO_SWITCH = "termo_switch"
+CONF_TERMO_TEMP_SENSOR = "termo_temp_sensor"
+CONF_TERMO_POWER_SENSOR = "termo_power_sensor"
+CONF_TERMO_NOMINAL_POWER = "termo_nominal_power"
 
 DEFAULT_WORKDAY = "binary_sensor.workday_sensor"
 
